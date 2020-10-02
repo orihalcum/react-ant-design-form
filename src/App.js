d2539548386832911503fed0579e0bc3743e4ad6
Dzulfan Fadli <dzulfanfadli@gmail.com>
2020-10-02 13:50:34 +0700

    login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FormLogin from './components/FormLogin';
+import { Row, Col } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: 60 }}>
+        <Row gutter={[60, 60]}>
+          <Col span={12}>
+            <FormLogin />
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
